bb05f4d58c35a896a8d83a4cdd80c27e727da366
salineroalex <alexsalineronegro@gmail.com>
2023-12-20 19:25:48 +0100

    Inheritance and relations for exam, student and mark

--- models/exam.py
+++ models/exam.py
@@ -1,12 +1,14 @@
 from odoo import fields, models, api
 
+
 class Exam(models.Model):
     _name = "bytebuddies.exam"
 
     id = fields.Integer()
     description = fields.Char()
-    filePath = fields.Char()
+    dateInit = fields.Date(string="dateInit")
     duration = fields.Integer()
-    # Marks ?? collection??
-    date = fields.Date()
-    # subject = fields.Subject()
+    filePath = fields.Char()
+    subject_id = fields.Many2one('bytebuddies.subject', string="Subject")
+    mark = fields.One2many('bytebuddies.mark', 'exam_id', ondelete='cascade', string="Mark")
+
--- models/user.py
+++ models/user.py
@@ -1,8 +1,18 @@
 from odoo import models, api, fields
 
+
 class User(models.Model):
     _name = "bytebuddies.user"
-    #_inherit = 'res.users'
+    # _inherit = 'res.users'
 
+    id = fields.Integer()
+    email = fields.Char()
+    name = fields.Char()
+    surname = fields.Char()
+    # password (encrypt)
     dateInit = fields.Date(string="dateInit")
     userType = fields.Selection(string="userType", selection=[("TEACHER", "Teacher"), ("STUDENT", "Student")])
+
+
+
+
