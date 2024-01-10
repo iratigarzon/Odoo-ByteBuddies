714adb99d7595591ab69a9221ebb8a38196a49bc
salineroalex <alexsalineronegro@gmail.com>
2024-01-10 12:26:53 +0100

    Solved security and implemented first views

--- models/exam.py
+++ models/exam.py
@@ -1,13 +1,15 @@
-from odoo import fields, models, api
+from odoo import models, api, fields
 
 
 class Exam(models.Model):
     _name = "bytebuddies.exam"
 
-    description = fields.Char()
+    id = fields.Integer("id")
+    name = fields.Char(string="Name")
+    description = fields.Char(string="Description")
     dateInit = fields.Date(string="dateInit")
-    duration = fields.Integer()
-    filePath = fields.Char()
+    duration = fields.Integer(string="duration")
+    filePath = fields.Char(string="File")
     subject_id = fields.Many2one('bytebuddies.subject', string="Subject")
-    mark = fields.One2many(comodel_name='bytebuddies.mark', inverse_name='exam_id', ondelete='cascade', string="Mark")
-
+    mark = fields.One2many(comodel_name='bytebuddies.mark', inverse_name='exam_id',
+                           string="Mark")  # ondelete='cascade',
